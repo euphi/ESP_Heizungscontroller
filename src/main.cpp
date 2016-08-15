3d42726830069ef0c9ada2d2d5023fa4622685dd
IanH <ian_web1@hubbertz.de>
2016-08-16 00:49:26 +0200

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 14.08.2016
+ *      Author: ian
+ */
+
+#include <Homie.hpp>
+
+#include <Homie.h>
+#include <RelaisNode.h>
+
+
+/* includes for Libraries, so platformio can find them */
+#include <Wire.h>
+#include <Sensors.h>
+#include <OLEDStatusIndicator.h>
+
+RelaisNode rel;
+OLEDStatusIndicator status;
+
+
+void eventHandler(HomieEvent event) {
+	status.Event(event);
+
+}
+void setup() {
+	Serial.begin(115200);
+	Serial.println("Up!");
+	Serial.flush();
+
+	status.setup();
+	Homie.disableResetTrigger();
+	Homie.onEvent(eventHandler);
+	Homie.enableLogging(true);
+	Homie.setup();
+}
+
+void loop() {
+	status.loop();
+	Homie.loop();
+}
