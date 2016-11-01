38fb7eb884b8040b2dea587e152e32a731a9811f
IanH <ian_web1@hubbertz.de>
2016-11-01 16:23:22 +0100

    Final release
    - also includes new .cproject due to pio update

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,20 @@
 
 /* includes for Libraries, so platformio can find them */
 #include <SensorNode.h> // TODO: Quirk to make pio find sensor.h when compiling HomieNodeCollection
+#include <Automaton.h>
 
 RelaisNode rel;
 
 void setup() {
-	Serial.begin(115200);
+	delay(200);
+	Serial.begin(74880);
 	Serial.println("Starting Heizungscontroller");
-	Serial.flush();
-
+	Homie.setLedPin(16, false);
 	Homie.disableResetTrigger();
 	LN.setLoglevel(LoggerNode::INFO);
-	Homie_setFirmware("HzngCtrl", "0.9");
-	Homie.setBroadcastHandler([](String level, String value) {LN.logf(__PRETTY_FUNCTION__,LoggerNode::INFO, "Broadcast: %s: %s", level.c_str(), value.c_str());return true;});
+	Homie_setFirmware("HzngCtrl", "1.0");
+	//Homie.setBroadcastHandler([](String level, String value) {LN.logf(__PRETTY_FUNCTION__,LoggerNode::INFO, "Broadcast: %s: %s", level.c_str(), value.c_str());return true;});
 	Homie.setup();
-	Serial.begin(115200);
-	Serial.println("Finished Setup");
-	Serial.flush();
 }
 
 void loop() {
